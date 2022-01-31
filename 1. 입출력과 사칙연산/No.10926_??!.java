1a109901e03e729a01e83d55d6ae9195286dedc3
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2022-01-31 21:11:54 +0900

    Create No.10926_??!.java

---
+++ 1. 입출력과 사칙연산/No.10926_??!.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10926
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println(sc.next() + "??!");
+        
+        sc.close();
+    }
+}
