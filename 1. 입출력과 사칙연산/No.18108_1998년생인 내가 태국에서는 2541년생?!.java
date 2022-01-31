a633f6df35f4f1c758966cf1b6a04aad988a523a
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2022-01-31 21:15:09 +0900

    Create No.18108_1998년생인 내가 태국에서는 2541년생?!.java

---
+++ 1. 입출력과 사칙연산/No.18108_1998년생인 내가 태국에서는 2541년생?!.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/18108
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println(sc.nextInt() - 543);
+        
+        sc.close();
+    }
+}
