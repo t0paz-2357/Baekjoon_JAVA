e057965dd31a1d6d21719e63f6458d971a6ab8ce
t0paz-2357 <90430489+t0paz-2357@users.noreply.github.com>
2022-01-31 16:29:46 +0900

    Create No.10171_고양이.java

---
+++ 1. 입출력과 사칙연산/No.10171_고양이.java
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/10171
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
